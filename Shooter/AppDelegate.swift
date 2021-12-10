684056217ea9dd4f261d4c858fc8bc1bfa70a978
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2021-12-10 11:22:06 -0300

    Remove unnecessary `application didFinishLaunchingWithOptions` on `AppDelegate`

--- Shooter/AppDelegate.swift
+++ Shooter/AppDelegate.swift
@@ -3,8 +3,4 @@ import UIKit
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
-    }
 }
